a7fb2656ab88d1152d1d86b7b2bd4ea0a292bf84
Karuppaiah Alagappan <karuppaiah.al@gmail.com>
2012-07-31 22:42:15 +0800

    part of sample project

--- Eshiah/src/com/eshiah/core/TextPlay.java
+++ Eshiah/src/com/eshiah/core/TextPlay.java
@@ -5,6 +5,9 @@ package com.eshiah.core;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.Gravity;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -16,6 +19,10 @@ import android.widget.ToggleButton;
  */
 public class TextPlay extends Activity{
 
+	Button chkCmd ;
+	ToggleButton passTog ;
+	EditText input ;
+	TextView display ;
 	/* (non-Javadoc)
 	 * @see android.app.Activity#onCreate(android.os.Bundle)
 	 */
@@ -24,11 +31,32 @@ public class TextPlay extends Activity{
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.text);
-		Button chkCmd = (Button)findViewById(R.id.bResults);
-		ToggleButton passTog = (ToggleButton)findViewById(R.id.tbPassword);
-		EditText input = (EditText)findViewById(R.id.etCommands);
-		TextView display = (TextView)findViewById(R.id.tvResults);
+		 chkCmd = (Button)findViewById(R.id.bResults);
+		 passTog = (ToggleButton)findViewById(R.id.tbPassword);
+		 input = (EditText)findViewById(R.id.etCommands);
+		 display = (TextView)findViewById(R.id.tvResults);
+		
+		
 	}
-	
+	public void clickTbPassword(View v){
+		if(passTog.isChecked()){
+			input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+		}else{
+			input.setInputType(InputType.TYPE_CLASS_TEXT);
+		}
+	}
 
+	public void clickBResults(View v){
+		String check = input.getText().toString().trim();
+		if(check.contentEquals("left")){
+			display.setGravity(Gravity.LEFT);
+		}else if(check.contentEquals("center")){
+			display.setGravity(Gravity.CENTER);
+		}else if(check.contentEquals("right")){
+			display.setGravity(Gravity.RIGHT);
+		}else if(check.contentEquals("blue")){
+			display.setGravity(Gravity.RIGHT);
+		}
+		
+	}
 }
